20486fe3ed2061c4236ca4afdc9c39a35459197e
LuizaAlex <poteca.luiza@gmail.com>
2024-10-10 18:43:37 +0300

    Initial commit

---
+++ app/src/main/java/com/luiza/contactsmanagerapplication/AddNewContactActivity.java
@@ -0,0 +1,52 @@
+package com.luiza.contactsmanagerapplication;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.databinding.DataBindingUtil;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.luiza.contactsmanagerapplication.R;
+import com.luiza.contactsmanagerapplication.databinding.ActivityAddNewContactBinding;
+
+
+public class AddNewContactActivity extends AppCompatActivity {
+
+    private ActivityAddNewContactBinding binding;
+    private AddNewContactClickHandler handler;
+    private Contacts contacts;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_new_contact);
+
+        contacts = new Contacts();
+        binding = DataBindingUtil.setContentView(
+                this,
+                R.layout.activity_add_new_contact
+        );
+
+
+        // VIew Model
+        MyViewModel myViewModel = new ViewModelProvider(this)
+                .get(MyViewModel.class);
+
+        handler = new AddNewContactClickHandler(
+                contacts,
+                this,
+                myViewModel
+        );
+
+
+        binding.setContact(contacts);
+        binding.setClickHandler(handler);
+
+
+    }
+}
